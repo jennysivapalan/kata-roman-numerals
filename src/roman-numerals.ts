4b913a2e23a1df5563d9dd5cc56d128242529331
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-11-01 16:24:19 +0000

    feat: Implements finding the single digit numerals for set of numbers

--- src/roman-numerals.ts
+++ src/roman-numerals.ts
@@ -1,3 +1,14 @@
-export function singleNumeral(x: number): string {
-  return "X";
+const singleNumeralToDigits: { num: number; romanNumeral: string }[] = [
+  { num: 1, romanNumeral: "I" },
+  { num: 5, romanNumeral: "V" },
+  { num: 10, romanNumeral: "X" },
+  { num: 50, romanNumeral: "L" },
+  { num: 100, romanNumeral: "C" },
+  { num: 500, romanNumeral: "D" },
+  { num: 1000, romanNumeral: "M" },
+];
+
+export function singleNumeral(x: number) {
+  const numeral = singleNumeralToDigits.find((value) => value.num === x);
+  return numeral ? numeral.romanNumeral : "No value found";
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,18 @@
 import { singleNumeral } from "../src/roman-numerals";
 
-describe("", () => {
+describe("test singleNumeral function", () => {
   //
   it("Returns the 1 letter roman numeral for associated number ", () => {
+    expect(singleNumeral(1)).toBe("I");
+    expect(singleNumeral(5)).toBe("V");
     expect(singleNumeral(10)).toBe("X");
+    expect(singleNumeral(50)).toBe("L");
+    expect(singleNumeral(100)).toBe("C");
+    expect(singleNumeral(500)).toBe("D");
+    expect(singleNumeral(1000)).toBe("M");
+  });
+
+  it("Returns No value found for value that does not exist", () => {
+    expect(singleNumeral(55)).toBe("No value found");
   });
 });
